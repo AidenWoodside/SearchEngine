92b1ffa9ddc2eb9a9c9eb713ad4ebde2c04b8889
AidenWoodside <woodside.a99@gmail.com>
2022-06-02 14:51:59 -0500

    Basic search

--- Engine.java
+++ Engine.java
@@ -1,8 +1,47 @@
-
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Engine {
-    public Engine()
+	
+	ArrayList<String> txt = new ArrayList<String>();
+	
+	//when object created, initialize index list
+	//find text file, initialize data structure to hold
+	//all of the elements of the text file
+    public Engine() throws FileNotFoundException
     {
-        System.out.println("Hi");
+    	 File file = new File("titles.txt");
+         Scanner sc = new Scanner(file);
+
+         //iterate through the text file
+         String line = "";
+         while(sc.hasNextLine())
+         {
+             line = sc.nextLine();
+             txt.add(line);
+         }
+    }
+    
+    //Method responsible for searching data structure for
+    //most relevant results based on input 'i'
+    public String[] getSearch(String i)
+    {
+    	
+    	String[] output = {"first", "second", "third"};
+    	return output;
+    }
+    
+    //return title at specific index
+    public String getIndex(int i)
+    {
+    	return txt.get(i);
+    }
+    
+    //return whole list
+    public ArrayList getList()
+    {
+    	return txt;
     }
 }
--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@
 //This main class will be used to iterate through
 //the user interaction. Takes user input in loop
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -10,22 +11,41 @@ import java.io.FileNotFoundException;
 public class Main {
 
     public static void main(String[] args) throws FileNotFoundException{
-
+    	
+    	//declare engine object
         Engine en = new Engine();
 
-        //construct the index file and file readedr
-        File file = new File("titles.txt");
-        Scanner sc = new Scanner(file);
-
-        //iterate through the text file
-        String line = "";
-        while(sc.hasNextLine())
-        {
-            line = sc.nextLine();
-
-            System.out.println(line);
-        }
-
+        //empty string that will hold user input
+        String input = "";
         
+        //declare the scanner to read user input
+        Scanner sc = new Scanner(System.in);
+        
+        //first prompt to user
+        System.out.println("Hello, what would you like to search for: \n");
+        input = sc.next();
+        
+        //declare empty array that will hold the three
+        //most relevant results based on search
+        String[] output = new String[3];
+        
+        
+        //Start the user input loop
+        while(!input.equals("-1"))
+        {
+        	//search for user input
+        	output = en.getSearch(input);
+        	
+        	//print the responses
+        	System.out.println("The most relevant responses are:");
+        	for(String x:output)
+        		System.out.println("\t"+ x);
+        	
+        	//prompt user for another search query 
+            System.out.println("\nwhat would you like to search for (type -1 to exit): \n");
+            input = sc.next();
+        }
+        
+        System.out.println("System Closing...");
     }
 }
