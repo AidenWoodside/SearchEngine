f7114c69c99c6a59393e8df00825cb60ebb83a45
AidenWoodside <woodside.a99@gmail.com>
2022-06-12 14:36:53 -0500

    Update to check website description

--- Engine.java
+++ Engine.java
@@ -9,7 +9,7 @@ import java.io.FileNotFoundException;
 public class Engine {
 	
 	//Initialize library to hold all of the books
-	private LinkedList<Book> library = new LinkedList<Book>();
+	private LinkedList<Website> library = new LinkedList<Website>();
 	
     public Engine() throws FileNotFoundException
     {
@@ -21,13 +21,14 @@ public class Engine {
 		Scanner sc = new Scanner(file);
 		
 		//iterate through the text file
-		String line = "";
+		String url = "";
+		String description = "";
 		while(sc.hasNextLine())
 		{
-		    line = sc.nextLine();
-		    
+		    url = sc.nextLine();
+		    description = sc.nextLine();
 		    //create new book object
-		    Book book = new Book(line, 0);
+		    Website book = new Website(url, 0, description);
 		    
 		    //add new object to library
 		    library.add(book);
@@ -54,7 +55,7 @@ public class Engine {
     	String[] output = new String[totalResults];
     	for(int i = 0; i < totalResults; i++)
     	{
-    		output[i] = library.get(i).getTitle();
+    		output[i] = library.get(i).getUrl();
     	}
     	
     	return output;
@@ -66,10 +67,10 @@ public class Engine {
     	String[] str = input.toLowerCase().split(" ", 20);
     	
     	//this hash set contains many prepositions and articles that
-    	//will be valued less when checking for relevance in titles
+    	//will be valued less when checking for relevance in description
     	HashSet<String> set = new HashSet<>(Arrays.asList("an", "a", "the", "of", "and", "in", "at", "for", "to", "on"));
     	
-    	//iterate through all of the titles
+    	//iterate through all of the descriptions
     	//check the first index of input
     	for(int i = 0; i < library.size(); i++)
     	{
@@ -77,47 +78,39 @@ public class Engine {
     		//with the user's search input
     		int relevance = 0;
     		
-    		//get title to check for relevance and normalize it to lowercase
+    		//get description to check for relevance
     		//to check against the input which has also bee normalized
-    		String title = library.get(i).getTitle().toLowerCase();
-    		
-    		//split title into array so it is easier to check for the relevance
-    		String[] split_title = title.split(" ", 50);
+    		HashSet<String> description = library.get(i).getHashedDescription();
     		
     		//iterate through the input array to check individual strings of 
     		//the input to all of the titles in the library
     		for(int j = 0; j < str.length; j++)
     		{
-    			
     			//initialize temporary string that will hold the value of the
     			//input string currently being checked against book title
     			String temp = str[j];
     			
-    			//iterate through every individual word within the title and
-    			//check to see if the current input value is within the title
-    			for(int k = 0; k < split_title.length; k++)
+    			//check if description contains word within input and set 
+    			//proper relevance based on if article or preposition
+    			if(description.contains(temp))
     			{
-    				
-    				//this checks to see if the input value is an article or
-    				//a preposition and assigns a lower relevancy value if true
-    				if(set.contains(temp) && temp.equals(split_title[k]))
+    				if(set.contains(temp))
     				{
     					relevance += 1;
     				}
-    				else if(temp.equals(split_title[k]))
+    				else
     				{
-    					relevance+=10;
+    					relevance += 10;
     				}
     			}
     		}
-    		
     		//set the relevance of the book
     		library.get(i).setRelevance(relevance);
     	}
     }
 
 
-    public static void sort(LinkedList<Book> list) 
+    public static void sort(LinkedList<Website> list) 
     {
         list.sort((o1, o2) -> Integer.compare(o2.getRelevance(), o1.getRelevance()));
     }
@@ -125,20 +118,45 @@ public class Engine {
 }
 
 
-class Book
+class Website
 {
-	private String title;
+	private String url;
+	private String description;
 	private int relevance;
 	
-	public Book(String title, int relevance) {
+	private HashSet<String> desc = new HashSet<String>();
+	
+	public Website(String title, int relevance, String description) {
 		super();
-		this.title = title;
+		
+		//initialize all of the internal attributes of webpage object
+		this.url = title;
 		this.relevance = relevance;
+		this.description = description;
+		String[] split = description.split(" ", 100);
+		
+		//create hashSet containing all the individual words of
+		//description so that checking for relevance is easier
+		for(String x : split) {
+			x = x.toLowerCase();
+			if(!desc.contains(x))
+				desc.add(x);
+		}
 	}
 	
-	public String getTitle()
+	public String getUrl()
 	{
-		return this.title;
+		return this.url;
+	}
+	
+	public String getDescription()
+	{
+		return this.description;
+	}
+	
+	public HashSet<String> getHashedDescription()
+	{
+		return desc;
 	}
 	
 	public int getRelevance()
@@ -149,5 +167,10 @@ class Book
 	public void setRelevance(int i)
 	{
 		this.relevance = i;
+	}
+	
+	public void setDescription(String description) 
+	{
+		this.description = description;
 	}
 }
